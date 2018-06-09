55c83a259598d249cbb99d8609e636e5d38dc868
Tuấn Anh <huutuananh1406@gmail.com>
2018-06-09 18:29:11 +0700

    django part 1

--- thanhspot/thanhspot/urls.py
+++ thanhspot/thanhspot/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from khohang import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
 ]
